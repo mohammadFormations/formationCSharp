9e40150268689d84d2f8c785f675da0bb130bfce
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2025-09-29 15:21:29 +0200

    test

--- formationCSharp/semaine1/Program.cs
+++ formationCSharp/semaine1/Program.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+
 
 namespace semaine1
 {
     internal class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello wossrld");
+            for (int i =0; i < 5; ++i)
+            {
+                Console.WriteLine(i);
+
+
+            }
+            for (int i = 0; i++ < 5;)
+            {
+                Console.WriteLine(i);
+
+
+            }
 
         }
     }
