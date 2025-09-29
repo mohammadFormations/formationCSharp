8b717f1e8ea561f57dfb4e06db23d352297ed097
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2025-09-29 17:08:13 +0200

    EXERCICES DU SERIE 1

--- formationCSharp/semaine1/Program.cs
+++ formationCSharp/semaine1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
-
+using Serie1;
 
 namespace semaine1
 {
@@ -15,20 +15,51 @@ namespace semaine1
 
         static void Main(string[] args)
         {
-            Console.WriteLine("hello wossrld");
-            for (int i =0; i < 5; ++i)
-            {
-                Console.WriteLine(i);
+            /*           ElementaryOperations.BasicOperation(3, 4, '+');
+                       ElementaryOperations.BasicOperation(6, 2, '/');
+                       ElementaryOperations.BasicOperation(3, 0, '/');
+                       ElementaryOperations.BasicOperation(6, 9, 'L');
+
+                       // integer devision 
+                       Console.WriteLine("integer devision ");
+
+                       ElementaryOperations.IntegerDivision(12, -4);
+                       ElementaryOperations.IntegerDivision(13, -4);
+                       ElementaryOperations.IntegerDivision(12, 0);
+
+                       // power 
+
+                       ElementaryOperations.Pow(2, -4);
+                       ElementaryOperations.Pow(3, 2);
+                       ElementaryOperations.Pow(-2, 3);*/
 
 
-            }
-            for (int i = 0; i++ < 5;)
-            {
-                Console.WriteLine(i);
+            // journe 
+            /*  SpeakingClock.Journee(-1);
+              SpeakingClock.Journee(0);
+              SpeakingClock.Journee(1);
+              SpeakingClock.Journee(5);
+              SpeakingClock.Journee(6);
+              SpeakingClock.Journee(11);
+              SpeakingClock.Journee(12);
+              SpeakingClock.Journee(13);
+              SpeakingClock.Journee(17);
+              SpeakingClock.Journee(18);
+              SpeakingClock.Journee(20);
+              SpeakingClock.Journee(23);
+              SpeakingClock.Journee(29);*/
+            /*
+                        Pyramid.PyramidConstruction(5, true);
+                        Pyramid.PyramidConstruction(5, false);*/
 
-
-            }
+            Console.WriteLine(Factorial.Factorial_(0));
+            Console.WriteLine(Factorial.Factorial_(1));
+            Console.WriteLine(Factorial.Factorial_(5));
+            Console.WriteLine(Factorial.FactorialRecursive(0));
+            Console.WriteLine(Factorial.FactorialRecursive(1));
+            Console.WriteLine(Factorial.FactorialRecursive(5));
 
         }
+
     }
 }
---
+++ formationCSharp/semaine1/serie1/ex4Factorials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie1
+{
+    public static class Factorial
+    {
+        public static int Factorial_(int n)
+        {
+            int result = 1;
+            for(int i =1; i < n + 1; i++)
+            {
+                result *= i;
+            }
+            return result;
+         }
+
+        public static int FactorialRecursive(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return n * FactorialRecursive(n- 1);
+        }
+    }
+}
