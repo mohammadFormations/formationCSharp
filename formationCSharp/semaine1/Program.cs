702490d1f0d406644d14e8156f2e45c97009d1d6
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2025-09-30 16:18:46 +0200

    test

--- formationCSharp/semaine1/Program.cs
+++ formationCSharp/semaine1/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
 using Serie1;
+using Serie2;
+using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
 
 namespace semaine1
 {
@@ -51,15 +54,72 @@ namespace semaine1
             /*
                         Pyramid.PyramidConstruction(5, true);
                         Pyramid.PyramidConstruction(5, false);*/
+            /*
+                        Console.WriteLine(Factorial.Factorial_(0));
+                        Console.WriteLine(Factorial.Factorial_(1));
+                        Console.WriteLine(Factorial.Factorial_(5));
+                        Console.WriteLine(Factorial.FactorialRecursive(0));
+                        Console.WriteLine(Factorial.FactorialRecursive(1));
+                        Console.WriteLine(Factorial.FactorialRecursive(5));
+            */
 
-            Console.WriteLine(Factorial.Factorial_(0));
-            Console.WriteLine(Factorial.Factorial_(1));
-            Console.WriteLine(Factorial.Factorial_(5));
-            Console.WriteLine(Factorial.FactorialRecursive(0));
-            Console.WriteLine(Factorial.FactorialRecursive(1));
-            Console.WriteLine(Factorial.FactorialRecursive(5));
+
+            /*
+             * serie 2
+             */
+            /*int[] tabs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            TasksTables.SommePrinter(tabs);
+
+            TasksTables.OperationPrinter(tabs, '+', 4);
+
+            TasksTables.OperationPrinter(tabs, '-', 4);
+
+            TasksTables.OperationPrinter(tabs, '*', 4);
+
+            TasksTables.OperationPrinter(tabs, '/', 4);
+
+            TasksTables.OperationPrinter(tabs, '/', 0);
+
+            TasksTables.OperationPrinter(tabs, 'L', 6);
+
+
+
+            int[] tab1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] tab2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            TasksTables.PrintTab(tab1, header:"tab 1 : ");
+            TasksTables.PrintTab(tab2, header:"tab 2 : ");
+            int[] concatinated = TasksTables.ConcatTab(tab1, tab2);
+            TasksTables.PrintTab(concatinated, header: "tab 1 + tab 2 : ");
+
+            int[] tab3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] tab4 = new int[] { };
+            TasksTables.PrintTab(tab1, header: "tab 1 : ");
+            TasksTables.PrintTab(tab2, header: "tab 2 : ");
+            int[] concatinated1 = TasksTables.ConcatTab(tab3, tab4);
+            TasksTables.PrintTab(concatinated1, header: "tab 1 + tab 2 : ");*/
+
+            /*            int[,] morpion = { { 1, 1, -1 }, { -1, -1, 0 }, { -1, -1, 0 } };
+                        Morpion.DisplayMorpion(morpion);
+                        int resultat = Morpion.CheckMorpion(morpion);
+                        Console.WriteLine(resultat);*/
+
+
+/*            int[] tab3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int result = Search.LinearSearch(tab3, -5);
+            Console.WriteLine(result);
+            Console.WriteLine(Search.BinarySearch(tab3, 6));
+            Console.WriteLine(Search.BinarySearch(tab3, 3));
+            Console.WriteLine(Search.BinarySearch(tab3, 0));
+            Console.WriteLine(Search.BinarySearch(tab3, 9));*/
+
+
+
+
 
         }
 
-    }
+        }
+
+
 }
---
+++ formationCSharp/semaine1/serie2/Ex3_Search.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie2
+{
+    public static class Search
+    {
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == valeur) return i;
+            }
+            //TODO
+            return -1;
+        }
+
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            int start = 0;
+            int end = tableau.Length - 1;
+            int mid = 0;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                // _ 8 4 5
+                Console.WriteLine($"hello{end}{mid}{start} ");
+                if (tableau[mid] < valeur) {
+                    start = mid + 1;
+                } else
+                {
+                    end = mid - 1;
+                }
+
+            }
+            Console.WriteLine($"hello{end}{mid}{start} ");
+            if (tableau[mid] == valeur) return mid;
+            return -1;
+        }
+    }
+}
